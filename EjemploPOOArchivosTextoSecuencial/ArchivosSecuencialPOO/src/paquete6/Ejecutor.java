8c3e7a59f084d6df0f63bfc4acab2abba0cea6a7
JeaenP <jpvpvalarezo@gmail.com>
2022-05-27 14:31:47 -0500

    nuevo

--- EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete6/Ejecutor.java
+++ EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete6/Ejecutor.java
@@ -10,5 +10,21 @@ package paquete6;
  * @author reroes
  */
 public class Ejecutor {
-    
+    public static void main(String[] args) {
+        String nombreArchivo ="Hospital.txt";
+        Hospital h1 = new Hospital("Hospital Regional",45,140.99);
+        Hospital h2 = new Hospital("Hospital UTPL",78,500.00);
+        Hospital h3 = new Hospital("Hospital Medilab",12,150.5);
+        ArchivoEscritura archivo = new ArchivoEscritura(nombreArchivo);
+        archivo.establecerRegistro(h1);
+        archivo.establecerSalida();
+        archivo.establecerRegistro(h2);
+        archivo.establecerSalida();
+        archivo.establecerRegistro(h3);
+        archivo.establecerSalida();
+        ArchivoLectura lectura = new ArchivoLectura(nombreArchivo);
+        lectura.establecerLista();
+        System.out.println(lectura);
+        lectura.cerrarArchivo();
+    }
 }
--- EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete6/Hospital.java
+++ EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete6/Hospital.java
@@ -13,6 +13,32 @@ public class Hospital {
     String nombre;
     int numeroCamas;
     double presupuesto;
+    
+    public Hospital(String nom, int numCam, double pre){
+        nombre = nom;
+        numeroCamas = numCam;
+        presupuesto = pre;
+    }
+    public void establecerNombre(String nom){
+        nombre = nom;
+    }
+    public String obtenerNombre(){
+        return nombre;
+    }
+    
+    public void establecerNumeroCamas(int numCam){
+        numeroCamas = numCam;
+    }
+    public int obtenerNumeroCamas(){
+        return numeroCamas;
+    }
+    
+    public void establecerPresupuesto(double pre){
+        presupuesto = pre;
+    }
+    public double obtenerPresupuesto(){
+        return presupuesto;
+    }
 }
 
 /* 
